4d835772c02e1254873f77788c83adf898e5ceab
YThilantha <yasitha.thilantha@cambio.lk>
2024-01-30 10:46:29 +0530

    initial commit

---
+++ src/main/kotlin/com/yas/microservicesInactionkotlin/MicroservicesInActionKotlinApplication.kt
@@ -0,0 +1,11 @@
+package com.yas.microservicesInactionkotlin
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class MicroservicesInActionKotlinApplication
+
+fun main(args: Array<String>) {
+	runApplication<MicroservicesInActionKotlinApplication>(*args)
+}
---
+++ src/test/kotlin/com/yas/microservicesInactionkotlin/MicroservicesInActionKotlinApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.yas.microservicesInactionkotlin
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class MicroservicesInActionKotlinApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
